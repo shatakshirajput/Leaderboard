b9020d9be644bd0feba7fc72244a6954880d495e
Arpit Gupta <arpitqwerty38@gmail.com>
2025-07-15 10:37:27 +0530

    fixing port

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:4000/api';
+const BASE_URL = import.meta.env.VITE_API_URL + '/api';
 
 export const fetchUsers = () => axios.get(`${BASE_URL}/leaderboard`);
 export const addUser = (name) => axios.post(`${BASE_URL}/users`, { name });
